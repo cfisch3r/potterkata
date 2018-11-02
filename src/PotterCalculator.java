9cbaa220bdb1d0dd601abf0b87459425d8be2477
Christian Fischer <cf@fisch3r.de>
2018-11-02 14:04:48 +0100

    two different books

--- src/PotterCalculator.java
+++ src/PotterCalculator.java
@@ -3,12 +3,20 @@ import java.math.BigDecimal;
 public class PotterCalculator {
 
     static final BigDecimal SINGLE_BOOK_PRICE = new BigDecimal(8);
+    private static final double DISCOUNT_RATE = 0.05;
 
     enum BOOKS {
-        BOOK1
+        BOOK2, BOOK1
     }
 
     public BigDecimal priceFor(BOOKS... books) {
-        return SINGLE_BOOK_PRICE.multiply(new BigDecimal(books.length));
+        if (books.length > 1 && books[0] != books[1]){
+            return basePrice(books.length).multiply(new BigDecimal(1- DISCOUNT_RATE));
+        }
+        return basePrice(books.length);
+    }
+
+    private BigDecimal basePrice(int count) {
+        return SINGLE_BOOK_PRICE.multiply(new BigDecimal(count));
     }
 }
--- src/PotterCalculatorTest.java
+++ src/PotterCalculatorTest.java
@@ -24,4 +24,10 @@ public class PotterCalculatorTest {
         BigDecimal price = calculator.priceFor(PotterCalculator.BOOKS.BOOK1,PotterCalculator.BOOKS.BOOK1);
         Assert.assertEquals(PotterCalculator.SINGLE_BOOK_PRICE.multiply(new BigDecimal(2)),price);
     }
+
+    @Test
+    public void two_different_books_get_discount() {
+        BigDecimal price = calculator.priceFor(PotterCalculator.BOOKS.BOOK1,PotterCalculator.BOOKS.BOOK2);
+        Assert.assertEquals(PotterCalculator.SINGLE_BOOK_PRICE.multiply(new BigDecimal(2)).multiply(new BigDecimal(0.95)),price);
+    }
 }
